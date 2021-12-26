ffb8710052610404ec16a51024bde8f5b34cdc7d
Sajal Gupta <guptasajal411@gmail.com>
2021-12-26 19:56:03 +0530

    feat: :rocket: Create and connect Register

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <div className="App">
-      hello world
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Register() {
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    async function submitRegisterForm(event) {
+        event.preventDefault();
+        const response = await fetch("http://localhost:3001/register", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify({
+                username, email, password
+            })
+        });
+        const data = await response;
+        console.log(data);
+    }
+
     return (
         <div>
-            
+            <h1>Register</h1>
+            <hr />
+            <form onSubmit={submitRegisterForm}>
+                <input
+                    value={username}
+                    onChange={(event) => { setUsername(event.target.value) }}
+                    type="text"
+                    placeholder="Username"
+                />
+                <br />
+                <input
+                    value={email}
+                    onChange={(event) => { setEmail(event.target.value) }}
+                    type="email"
+                    placeholder="Email address"
+                />
+                <br />
+                <input
+                    value={password}
+                    onChange={(event) => { setPassword(event.target.value) }}
+                    type="password"
+                    placeholder="Password"
+                />
+                <br />
+                <button type="submit">Submit</button>
+            </form>
         </div>
     )
 }
